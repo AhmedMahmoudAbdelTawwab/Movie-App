09d6d18f98a81d6320079461c7972993a1eee198
ahmed mahmoud <ahmedmahmous876234@gmail.com>
2026-05-03 14:44:44 +0300

    Implement bottom navigation bar and update main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/bottom_nav_bar/bottom_nav_bar_widget.dart';
 
 void main() {
   runApp(MovieApp());
 }
 
-class MovieApp extends StatelessWidget {
+class MovieApp extends StatefulWidget {
   const MovieApp({super.key});
 
   @override
+  State<MovieApp> createState() => _MovieAppState();
+}
+
+class _MovieAppState extends State<MovieApp> {
+  int stackIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        //  bottomNavigationBar: BottomNavBarWidget,
+      ),
+    );
   }
 }
